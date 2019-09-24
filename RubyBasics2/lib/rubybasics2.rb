2e8176612507f33465eaff36ae060f136e2e0b3f
Sadie Anderson <sander94@uncc.edu>
2019-09-24 22:19:08 +0000

    Lab 9 initial commit

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -2,15 +2,26 @@
 
 # Part I
 def hello(name)
-  # YOUR CODE HERE
+    "Hello, " + name
 end
 
 # Part II
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
 # Part III
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
+  def binary_multiple_of_4? s
+    is = s.to_i
+    if s == "0"
+      return true
+    elsif /[a-zA-Z^$3-9*]/.match(s)
+      return false
+    else 
+      if /^[10]*00$/.match(s) && is % 2 == 0
+        return true
+      else
+        return false
+      end
+    end
+  end
