e8e594cbd233f34ceaa2fb83a55d1d595d6ac3d3
Sadie Anderson <sander94@uncc.edu>
2019-10-03 19:53:13 +0000

    Lab 10 initial commit

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,9 +1,13 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.gsub(/\W/,' ').downcase == self.gsub(/\W/, ' '.downcase.reverse)
   end
+  
   def count_words
-    # your code here
+   counter = Hash.new(0)
+   words = self.downcase.scan(/\W+/)
+   words.each { |word| counter[word] += 1}
+   counter
   end
   def anagram_groups
     # your code here
