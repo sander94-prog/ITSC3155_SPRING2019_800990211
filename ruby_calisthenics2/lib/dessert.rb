135e683d4a64fc8fdb27ccef176b717dc5b7bf14
Sadie Anderson <sander94@uncc.edu>
2019-10-04 18:49:53 +0000

    Ruby Cali

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -1,19 +1,28 @@
 class Dessert
-  # add code for setters and getters
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    if(calories < 200 )
+      return true
+    end
+    return false
   end
   def delicious?
-    # your code here
+    if(@flavor == "licorice")
+      return false
+    end 
+    return true
   end
 end
 
 class JellyBean < Dessert
-  # add code for setters and getters
+  attr_accessor :flavor
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    @calories = 5
+    @name = flavor + " jelly bean"
   end
 end
