86ec9aad96788772586968f535467b3d3f0b30cd
Sadie Anderson <sander94@uncc.edu>
2019-11-04 23:34:02 +0000

    initial commit

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,19 @@
 # Ruby Basics Part 3
 
 class BookInStock
-# YOUR CODE HERE
+ # YOUR CODE HERE
+    attr_accessor :isbn, :price
+    def initialize(isbn, price)
+        if(isbn.empty?)
+            raise ArgumentError, 'Argument is not valid'
+        end
+        if(price <= 0)
+            raise ArgumentError, 'Argument is not valid'
+        end
+        @isbn = isbn
+        @price = price
+    end 
+    def price_as_string
+        format("$%.2f", @price)
+    end
 end
