754e5d0f079554d7df8d761392124c780f6c63d6
Sadie Anderson <sander94@uncc.edu>
2019-11-04 21:12:06 +0000

    Lab 9 Done

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,16 +1,47 @@
 module FunWithStrings
   def palindrome?
-    self.gsub(/\W/,' ').downcase == self.gsub(/\W/, ' '.downcase.reverse)
-  end
-  
-  def count_words
-   counter = Hash.new(0)
-   words = self.downcase.scan(/\W+/)
-   words.each { |word| counter[word] += 1}
-   counter
-  end
-  def anagram_groups
     # your code here
+    self.downcase!
+    self.gsub!(/[^a-z]/i,"")
+    newstring = self.reverse
+    if newstring.eql? self
+      return true
+    end
+  end
+  def count_words 
+    # your code here
+    self.downcase!
+    self.gsub!(/[^a-z\s+]/i,"")
+    words = self.split(" ")
+    hash = Hash.new
+    i = 0
+    words.each do |x|
+      if(hash.has_key? words[i])
+        hash[words[i]] = hash[words[i]] + 1
+        i = i + 1
+      else
+        hash[words[i]] = 1
+        i = i + 1
+      end
+    end
+    return hash
+  end
+  def anagram_groups 
+    # your code here
+    teststring = self
+    arraystring = teststring.split(" ")
+    newHash = Hash.new { |hash, key| hash[key] = Array.new }
+    arraystring.each do |x|
+      newHash[x.downcase.chars.sort].push(x)
+    end
+    hashkey = newHash.keys
+    arrayofstring = []
+    h = 0
+    hashkey.each do |c|
+        arrayofstring[h] = newHash[c]
+        h = h + 1
+    end
+    return arrayofstring
   end
 end
 
